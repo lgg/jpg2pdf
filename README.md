c69949054810309530073c0d20fd9b79cb617e58
lgg <fed777os@gmail.com>
2021-10-27 03:45:07 +0300

    [WIP] add draft update

--- README.md
+++ README.md
@@ -1,17 +1,46 @@
-# JPG-to-PDF
+# jpg2pdf
+
+readme is WIP
+
+---
+
+add info about pdunite: https://stackoverflow.com/questions/2507766/merge-convert-multiple-pdf-files-into-one-pdf
+
+update here:
+
+---
+
 Simple Python script to convert a set of images to PDF and combine them into one single PDF file within seconds.
-<br /><br />
+
 ![screenshot_1](https://user-images.githubusercontent.com/29803330/46648492-f338c000-cbb2-11e8-908d-05928f285720.jpg)
 
-Do you have a set of pictures from the class topper's notes but find it difficult studying from it because of the jpg format? JPG to PDF is a simple Python script which converts all the images into PDF format and the merges all the images as one single PDF. In case you have an image which was accidentally clicked in landscape mode, the script will automatically convert it into portrait mode. <br />
-**Note : The script works only for pictures clicked in portrait mode.** <br />
+Do you have a set of pictures from the class topper's notes but find it difficult studying from it because of the jpg format? JPG to PDF is a simple Python script which converts all the images into PDF format and the merges all the images as one single PDF. In case you have an image which was accidentally clicked in landscape mode, the script will automatically convert it into portrait mode.
 
-## Working
+**Note : The script works only for pictures clicked in portrait mode.**
+
+## Installation
+
+
+## Usage
+
 The script only takes two inputs.
 1. The directory where the images are stored.
 2. The name of the output PDF file. 
 
 ![image](https://user-images.githubusercontent.com/29803330/46648841-71499680-cbb4-11e8-91e6-e5cc0e9b3e36.png)
-<br /><br />
+
 **Note : The directory should not contain any subdirectories having images in them, i.e., the diretory should contain only the images to be converted to PDF. The output PDF is saved in the same directory.** <br /><br />
 Demo video of the script : https://drive.google.com/open?id=1HsPsVmQhWT5HVDtXKeIeiM9pCGJ9wvNc
+
+## ToDo
+
+* add single image convert
+* add horizontal orientation support
+
+## License
+
+* MIT, 2021
+* 2021 update by [@lgg](https://github.com/lgg)
+* [Original repo](https://github.com/praneetk2704/JPG-to-PDF) by [@praneetk2704](https://github.com/praneetk2704)
+* [cli-mode PR](https://github.com/praneetk2704/JPG-to-PDF/pull/2) by [@Fiereu](https://github.com/Fiereu)
+
--- jpg2pdf.py
+++ jpg2pdf.py
@@ -5,24 +5,40 @@ import sys
 import getopt
 
 pdf = FPDF()
-imagelist = []                                                 # Contains the list of all images to be converted to PDF.
+imagelist = []  # Contains the list of all images to be converted to PDF.
 
+help_text = {
+    'usage_cmd_single': 'jpg2pdf -s <source_image>',
+    'usage_cmd_folder': 'jpg2pdf -f <source_images_folder> -n <output_pdf_name> -e <source_image_extension>',
+    'usage_text': '\n123',
+    'about': '\nSimple Python script to create a PDF file from a set of images\n\nSource code: https://github.com/lgg/jpg2pdf',
+}
+
+# TODO: add flags: j for JPG/jfif/JPEG and p for PNG instead of -e
+# TODO: add wrong input command parser
+# TODO: add -s flag for single file
+# TODO: fix all text output
+# TODO: check what happen when send horizontal images
+# TODO: add flag for vertical/horizontal orientation
 
 # --------------- USER INPUT -------------------- #
 
-folder = ""                                                    # Folder containing all the images.
-name = ""  
-extension = ""                                                    # Name of the output PDF file.
+folder = ""  # Folder containing all the images.
+name = ""
+extension = ""  # Name of the output PDF file.
+
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"hf:n:e:",["folder=","name=","extension="])
+        opts, args = getopt.getopt(argv, "hf:n:e:", ["folder=", "name=", "extension="])
     except getopt.GetoptError:
-        print("JPG to PDF -f <folder> -n <name> -e <extension>")
+        print(help_text['usage_cmd'])
         sys.exit(2)
     for opt, arg in opts:
         if opt == "-h":
-            print("JPG to PDF\n -f <folder>\n -n <name>\n -e <extension>\n")
+            print(help_text['usage_cmd'])
+            print(help_text['usage_text'])
+            print(help_text['about'])
             sys.exit()
         elif opt in ("-f", "--folder"):
             folder = arg
@@ -30,44 +46,43 @@ def main(argv):
             name = arg
         elif opt in ("-e", "--extension"):
             extension = arg
-    	
+
     # ------------- ADD ALL THE IMAGES IN A LIST ------------- #
     for dirpath, dirnames, filenames in os.walk(folder):
-    	for filename in [f for f in filenames if f.endswith(extension) or f.lower().endswith(extension)]:
-    		full_path = os.path.join(dirpath, filename)
-    		imagelist.append(full_path)
-    
-    #for dirpath, dirnames, filenames in os.walk(folder):
-    #	for filename in [f for f in filenames if f.endswith(".jpeg")]:
-    #		full_path = os.path.join(dirpath, filename)
-    #		imagelist.append(full_path)
-    
-    imagelist.sort()                                               # Sort the images by name.
+        for filename in [f for f in filenames if f.endswith(extension) or f.lower().endswith(extension)]:
+            full_path = os.path.join(dirpath, filename)
+            imagelist.append(full_path)
+
+    imagelist.sort() # Sort the images by name.
     for i in range(0, len(imagelist)):
-    	print(imagelist[i])
-    
+        print(imagelist[i])
+
     # --------------- ROTATE ANY LANDSCAPE MODE IMAGE IF PRESENT ----------------- #
-    
+
     for i in range(0, len(imagelist)):
-    	im1 = Image.open(imagelist[i])                             # Open the image.
-    	width, height = im1.size                                   # Get the width and height of that image.
-    	if width > height:
-    		im2 = im1.transpose(Image.ROTATE_270)                  # If width > height, rotate the image.
-    		os.remove(imagelist[i])                                # Delete the previous image.
-    		im2.save(imagelist[i])                                 # Save the rotated image.
-    		# im.save
-    
+        im1 = Image.open(imagelist[i]) # Open the image.
+        width, height = im1.size  # Get the width and height of that image.
+        ## TODO: add flag to disable auto-rotate (and set rotation left/right)
+        if width > height:
+            im2 = im1.transpose(Image.ROTATE_270) # If width > height, rotate the image.
+            os.remove(imagelist[i]) # Delete the previous image.
+            im2.save(imagelist[i]) # Save the rotated image.
+        # im.save
+
     print("\nFound " + str(len(imagelist)) + " image files. Converting to PDF....\n")
-    
-    
+
     # -------------- CONVERT TO PDF ------------ #
-    
+
     for image in imagelist:
-    	pdf.add_page()
-    	pdf.image(image, 0, 0, 210, 297)                           # 210 and 297 are the dimensions of an A4 size sheet.
-    
-    pdf.output(folder +"/"+ name+".pdf", "F")                                 # Save the PDF.
-    
-    print("PDF generated successfully!\nAnd saved as "+folder +"/"+ name+".pdf")
+        pdf.add_page()
+        pdf.image(image, 0, 0, 210, 297)  # 210 and 297 are the dimensions of an A4 size sheet.
+
+    pdf.output(folder + "/" + name + ".pdf",
+               "F")  # TODO: convert to path join # Save the PDF.
+
+    print(
+        "PDF generated successfully!\nAnd saved as " + folder + "/" + name + ".pdf") # TODO move to format and variables
+
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
